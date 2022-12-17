c18e1584f5c8b3ae8ddaeb813ea8bdd11b43ba0f
MSafyan <msafyan080@gmail.com>
2022-12-17 22:34:30 +0500

    port set

--- server/app.js
+++ server/app.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 app.use('/api', routes);
 
 app.use('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 5000;
